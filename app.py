518eada018f6e671953f03c00ef0483f56b66f14
Sherilyn Koshy <sherilyn.koshy@gmail.com>
2023-08-24 15:15:31 -0400

    add model, routes, crud operations

--- app.py
+++ app.py
@@ -6,29 +6,77 @@ db = PostgresqlDatabase(
     "seinfeld", user="postgres", password="", host="localhost", port=5432
 )
 
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Character(BaseModel):
+    name = CharField()
+    gender = CharField()
+    occupation = CharField()
+    portrayedby = CharField()
+
+
+db.connect()
+db.drop_tables([Character])
+db.create_tables([Character])
+
+Character(
+    name="Jerry",
+    gender="male",
+    occupation="comedian",
+    portrayedby="Jerry Seinfeld",
+).save()
+Character(
+    name="George",
+    gender="male",
+    occupation="architect",
+    portrayedby="Jason Alexander",
+).save()
+Character(
+    name="Elaine",
+    gender="female",
+    occupation="writer",
+    portrayedby="Julia Louis-Dreyfus",
+).save()
+Character(
+    name="Kramer",
+    gender="male",
+    occupation="unemployed",
+    portrayedby="Michael Richards",
+).save()
+
+
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    app.run(port=5000, debug=True)
-
-
-@app.route("/endpoint", methods=["GET", "PUT", "POST", "DELETE"])
-def endpoint():
+@app.route("/character", methods=["GET", "POST"])
+@app.route("/character/<id>", methods=["GET", "PUT", "DELETE"])
+def endpoint(id=None):
     if request.method == "GET":
-        return "GET"
+        if id:
+            return jsonify(model_to_dict(Character.get_by_id(id)))
+    else:
+        characters = []
+        for character in Character.select():
+            characters.append(model_to_dict(character))
+        return jsonify(characters)
 
     if request.method == "PUT":
-        return "PUT"
+        body = request.get_json()
+        Character.update(body).where(Character.id == id).execute()
+        return "Character {id} updated."
 
     if request.method == "POST":
-        return "POST"
+        add_character = dict_to_model(Character, request.get_json())
+        add_character.save()
+        return "Character {id} added."
 
     if request.method == "DELETE":
-        return "DELETE"
+        Character.delete().where(Character.id == id).execute()
+        return "Character {id} deleted."
 
 
-@app.route("/json")
-def json():
-    return jsonify({"key": "value", "listkey": [1, 2, 3], "isbool": True})
+app.run(port=5000, debug=True)
